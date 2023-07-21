9c33a0460b95745018fe054352f50eb39063f317
larsmunaf <lmu001@munaf.de>
2023-07-21 11:59:49 +0200

    Added init.lua

---
+++ config/nvim/lua/options.lua
@@ -0,0 +1,18 @@
+-- editor
+vim.opt.cursorline = true
+
+vim.opt.swapfile = false
+
+vim.opt.relativenumber = true
+
+vim.opt.signcolumn = "yes"
+
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+
+
+-- search
+vim.opt.hlsearch = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
